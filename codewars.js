873fddfd3161c1da6c03216453e56da794d47497
Brian Archibald <brianallenarchibald@gmail.com>
2018-05-26 16:41:20 -0500

    array.from / join / sort / split / ternary

--- codewars.js
+++ codewars.js
@@ -181,6 +181,84 @@ function splitAndMerge(str, sp) {
 function cutIt(arr){
   var min = Math.min(...arr.map(({ length }) => length));
   //map over array slice it by min.
-    
-  
 }
+
+// Take 2 strings s1 and s2 including only letters from ato z.
+//  Return a new sorted string, the longest possible, containing distinct letters,
+// each taken only once - coming from s1 or s2. #Examples: 
+// ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
+
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+or
+
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2)).sort().join('');
+}
+
+or 
+
+function longest(s1, s2) {
+  s3 = s1 + s2;
+  s4 = s3.split("");
+  s4 = s4.sort().filter(function(element, index, array){
+    return element !== array[index - 1];
+  });
+  return s4.join("");
+}
+
+///////////////////////////////////////////////////////////
+
+// Give you a function ```animal```, accept 1 parameter:```obj``` like this:
+// ```
+// {name:"dog",legs:4,color:"white"}
+// ``` 
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+////////////////////////////////////////////////////
+Coding in function ```trueOrFalse```, function accept 1  parameters:```val```, 
+try to use the conditional statement if...else, 
+if ```val``` value is false (val==false or it can convert to false),
+ should return a string "false", if not, return a string "true".
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+In the end, click "Submit" to submit your code pass this kata.
+
+
+function trueOrFalse(val){
+  return Boolean(val).toString();
+}
+
+////////////////////////////////////////////////////////
+
+Complete function saleHotdogs/SaleHotDogs, function accept 1 parameters:n, 
+n is the number of customers to buy hotdogs, 
+different numbers have different prices (refer to the following table), 
+return a number that the customer need to pay how much money.
++---------------+-------------+
+|  numbers n    | price(cents)|
++---------------+-------------+
+|n<5            |    100      |
++---------------+-------------+
+|n>=5 and n<10  |     95      |
++---------------+-------------+
+|n>=10          |     90      |
++---------------+-------------+
+You can use if..else or ternary operator to complete it.
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+In the end, click "Submit" to submit your code pass this kata.
+
+
+function saleHotdogs(n){
+  return n*(n<5?100:n<10?95:90);
+}
+
+/////////////////////////////////////////////////////////////////
+
